285498428103a38efd7890bf41ba79cf568050af
juliadang <juliadang91@gmail.com>
2016-09-30 09:25:52 +0200

    Log in button is now redirecting

---
+++ PingAppAndroid/.localhistory/c/users/administrator/source/repos/academic-work-gothenburg/pingappandroid/pingappandroid/1475158451$mainactivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace PingAppAndroid
+{
+    [Activity(Label = "PingAppAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            Button Login = FindViewById<Button>(Resource.Id.button1);
+            Login.Click += redirectToIndex;
+        }
+
+        private void redirectToIndex(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.Index);
+        }
+    }
+}
+
