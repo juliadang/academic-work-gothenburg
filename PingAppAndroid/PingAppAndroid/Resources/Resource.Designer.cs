7fcfbda8fb4bdf607f23d57c9cbd86b23498dabc
linneateigen <linneateigen@hotmail.com>
2016-10-05 13:46:48 +0200

    login.. bara designen ändrad

--- PingAppAndroid/PingAppAndroid/Resources/Resource.Designer.cs
+++ PingAppAndroid/PingAppAndroid/Resources/Resource.Designer.cs
@@ -60,20 +60,20 @@ namespace PingAppAndroid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050008
-			public const int FriendListBtn = 2131034120;
+			// aapt resource value: 0x7f05000a
+			public const int FriendListBtn = 2131034122;
 			
-			// aapt resource value: 0x7f05000c
-			public const int LogOutBtn = 2131034124;
-			
-			// aapt resource value: 0x7f050007
-			public const int NotificationsBtn = 2131034119;
+			// aapt resource value: 0x7f05000e
+			public const int LogOutBtn = 2131034126;
 			
 			// aapt resource value: 0x7f050009
-			public const int ProfileBtn = 2131034121;
+			public const int NotificationsBtn = 2131034121;
 			
-			// aapt resource value: 0x7f05000d
-			public const int Settings = 2131034125;
+			// aapt resource value: 0x7f05000b
+			public const int ProfileBtn = 2131034123;
+			
+			// aapt resource value: 0x7f05000f
+			public const int Settings = 2131034127;
 			
 			// aapt resource value: 0x7f050000
 			public const int TxtUserName = 2131034112;
@@ -90,17 +90,23 @@ namespace PingAppAndroid
 			// aapt resource value: 0x7f050002
 			public const int friendList = 2131034114;
 			
-			// aapt resource value: 0x7f05000a
-			public const int scrollView1 = 2131034122;
+			// aapt resource value: 0x7f05000c
+			public const int scrollView1 = 2131034124;
 			
-			// aapt resource value: 0x7f05000b
-			public const int scrollView2 = 2131034123;
+			// aapt resource value: 0x7f05000d
+			public const int scrollView2 = 2131034125;
+			
+			// aapt resource value: 0x7f050007
+			public const int signInFBButton = 2131034119;
 			
 			// aapt resource value: 0x7f050001
 			public const int textView1 = 2131034113;
 			
 			// aapt resource value: 0x7f050006
-			public const int toolbar1 = 2131034118;
+			public const int textView2 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int toolbar1 = 2131034120;
 			
 			static Id()
 			{
