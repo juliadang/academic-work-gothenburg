ca1daf41a2e4785d3e6d039de2946e965161ca03
Toni Lee <contact.tonilee@gmail.com>
2016-10-13 14:46:24 +0200

    Fått WebAPI:et att funka

--- PingAppAndroid/SimpleWebApi/Controllers/AccountController.cs
+++ PingAppAndroid/SimpleWebApi/Controllers/AccountController.cs
@@ -11,12 +11,46 @@ namespace SimpleWebApi.Controllers
 {
     public class AccountController : ApiController
     {
-        [HttpGet]
-        public IHttpActionResult Register(string name, string password)
-        {
-            User user = new User { UserName = name, Password = password };
+        //PingdbContext _dbContext;
+        //DatabaseUtils _dm;
+        //public AccountController(PingdbContext context)
+        //{
+        //    _dbContext = context;
+        //    _dm = new DatabaseUtils(context);
+        //    _dbContext.Database.ensurecreated();
+        //}
 
+        DatabaseUtils _dm = new DatabaseUtils(new PingdbContext());
+
+        [HttpGet]
+        public IHttpActionResult Register(string name, string password, string email)
+        {
+            PingUsers user = new PingUsers { Username = name, Password = password, Email = email };
+
+            _dm.RegisterUser(user);
             return Ok(JsonConvert.SerializeObject(user));
         }
+        
+        public IHttpActionResult Login()
+        {
+            List<PingUsers> users =_dm.GetUsers();
+
+            return Ok(JsonConvert.SerializeObject(users));
+        }
+        public IHttpActionResult EditProfile()
+        {
+
+        }
+        public IHttpActionResult GetFriend()
+        {
+
+        }
+        
+        public IHttpActionResult AddFriend()
+        {
+
+        }
+        public IHttpActionResult RemoveFriend()
+
     }
 }
---
+++ PingAppAndroid/SimpleWebApi/Models/PingdbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleWebApi.Models
+{
+    public class PingdbContext : DbContext
+    {
+        public PingdbContext() : base("PingdbContext")
+        {
+        }
+        public DbSet<PingUsers> PingUsers { get; set; }
+    }
+}
